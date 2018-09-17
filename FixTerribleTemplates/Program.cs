5f06cb367e6e1f5af21a90baad74e54b531ff3ff
David <dai@dai.me>
2018-09-16 17:50:24 -0700

    Adding project and old PowerShell version.

--- FixTerribleTemplates/Program.cs
+++ FixTerribleTemplates/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,57 @@ using System.Threading.Tasks;
 
 namespace FixTerribleTemplates
 {
-	class Program
+	public static class Program
 	{
-		static void Main( string[] args )
+		public static void Main( String[] args )
 		{
+			// CBA to reimplement vswhere.exe: https://github.com/Microsoft/vswhere
+			// Do it manually or with command-line args:
+
+			List<String> fileNames;
+			{
+				DirectoryInfo[] roots = new DirectoryInfo[]
+				{
+					new DirectoryInfo( @"C:\Program Files (x86)\Microsoft Visual Studio 15.0" ),
+					new DirectoryInfo( @"C:\Users\David\Apps\Visual Studio 2017\Templates" ),
+					// new DirectoryInfo( @"C:\Users\David\AppData\Local\Microsoft\VisualStudio\15.0_e7c3c18b" )
+				};
+
+				fileNames = GetFileNames( roots );
+			}
+		}
+
+		public static List<String> GetFileNames(IEnumerable<DirectoryInfo> rootDirectories)
+		{
+			List<String> fileNames = new List<String>();
+
+			foreach( DirectoryInfo root in rootDirectories )
+			{
+				if( root.Exists )
+				{
+					GetFileNames( root.Name.Length, root, pathContainsTemplates: false, fileNames );
+				}
+			}
+
+			return fileNames;
+		}
+
+		private static void GetFileNames(Int32 rootPrefixLength, DirectoryInfo directory, Boolean pathContainsTemplates, List<String> fileNames)
+		{
+			Boolean isTemplateDirectory = pathContainsTemplates || directory.Name.IndexOf( "templates", StringComparison.OrdinalIgnoreCase ) > -1;
+
+			foreach( DirectoryInfo child in directory.GetDirectories() )
+			{
+				GetFileNames( rootPrefixLength, child, isTemplateDirectory, fileNames );
+			}
+
+			if( isTemplateDirectory )
+			{
+				foreach( FileInfo file in directory.GetFiles("*.cs") )
+				{
+					fileNames.Add( file.FullName.Substring( startIndex: rootPrefixLength );
+				}
+			}
 		}
 	}
 }
---
+++ _PowerShell/Update-VS2017Templates.ps1
@@ -0,0 +1,215 @@
+﻿Set-StrictMode -Version 2.0
+$ErrorActionPreference = "Stop"
+
+###########################################
+## Summary
+
+# This script changes the namespace imports in your C# ItemTemplates and ProjectTemplates and improves your default AssemblyInfo.cs files.
+# It is designed for Visual Studio 2017, but should work with all Visual Studio versions going back as far as Visual Studio 2002, just set $vsCommon7IDE correctly.
+
+###########################################
+## Details
+
+# This script does the following:
+# 1. Copies all C# files from your Visual Studio ItemTemplates and ProjectTemplates directories in the VS installation folder.
+# 2. Adds `using` namespace imports to every C# file that already has at least 1 other `using` statement:
+#   1. System.Globalization
+#   2. System.Text
+# 3. Changes `AssemblyInfo.cs` files:
+#   1. Adding `using System;`
+#   2. Adding `[assembly: CLSCompliant( isCompliant: true )]`
+#   3. Changes `[assembly: AssemblyVersion("1.0.0.0")]` to `[assembly: AssemblyVersion("1.0.*")]`
+#   4. Comments-out `[assembly: AssemblyFileVersion("1.0.0.0")]` (the compiler uses AssemblyVersion for AssemblyFileVersion if AssemblyFileVersion is not present)
+# 4. This script does not copy the files back into Program Files because it needs elevated permissions for that; but you can just cut+paste them with File Explorer.
+
+###########################################
+## Instructions
+
+$vsCommon7IDE = "auto" # Set to 'auto' to autodiscover the VS2017 installation location. Requires VS2017 15.2 or later. Otherwise set it to the "Common7\IDE" directory's absolute path.
+$useGit       = $false  # Set this to `$true` to create a repo with the original files and then adds the changes so you can see the diffs. Ensure 'git' works in your PowerShell first.
+
+###########################################
+## You don't need to edit anything below this line.
+
+If( $vsCommon7IDE -eq "auto" ) {
+
+	$vsWherePath = "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+	$vsWherePath = [System.Environment]::ExpandEnvironmentVariables( $vsWherePath )
+
+	If( -not (Test-Path $vsWherePath) ) {
+		Throw "Could not find vswhere.exe. Do you have Visual Studio 2017 version 15.2 or later installed? Set 'vsTemplatePath' in this script to the directory name otherwise."
+	}
+
+	$vsInstallPath = & $vsWherePath | Select-String "installationPath: (.+)" | %{ $_.Matches.Groups[1].Value }
+
+	If( -not (Test-Path $vsInstallPath) ) {
+		Throw "`"$vsInstallPath`" does not exist."
+	}
+
+	$vsCommon7IDE = (Join-Path -Path $vsInstallPath -ChildPath "Common7\IDE")
+}
+
+$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+
+$fileCount = 0
+
+function Update-CSharpItemTemplate {
+	[CmdletBinding()]
+	param(
+		[Parameter(ValueFromPipeline)] [System.IO.FileInfo]$fileInfo,
+		[string]$workingDirectoryPath
+	)
+
+	Begin { }
+	Process {
+	
+		$encoding = Get-FileEncoding $fileInfo.FullName -DefaultEncoding $Utf8NoBomEncoding
+		$newFileContent = $null
+
+		$fileContent = Get-Content $fileInfo.FullName -Raw # -Raw returns a single string, not an array of lines
+		$newFileContent = $fileContent
+
+		$relativeFileName = $fileInfo.FullName.Substring( $workingDirectoryPath.Length )
+
+		If( $newFileContent -eq $null -or $newFileContent.Length -eq 0 ) {
+		
+			Write-Host "Skipped empty file: $relativeFileName"
+			return
+		}
+		ElseIf( $newFileContent.Length -lt 25 ) {
+			
+			Write-Host "Skipped short file: $relativeFileName, Length: $($fileContent.Length)"
+			return
+		}
+
+		If( -not $newFileContent.Trim().StartsWith("using") ) {
+			
+			Write-Host "Skipped non-using file: $relativeFileName"
+			return
+		}
+
+		If( $fileInfo.Name -ieq "AssemblyInfo.cs" ) { # `-ieq` is Case-insensitive string equality
+			
+			If( $newFileContent -notmatch "using System;" ) {
+			
+				$newFileContent = "using System;`r`n" + $newFileContent
+			}
+
+			# these two are string-replace operations, so no need to do a match check first:
+			$newFileContent = $newFileContent -replace "\[assembly: AssemblyVersion\(`"1\.0\.0\.0`"\)\]", "[assembly: AssemblyVersion(`"1.0.*`")]"
+			$newFileContent = $newFileContent -replace "`n\[assembly: AssemblyFile", "`n//[assembly: AssemblyFile"
+			
+
+			If( $newFileContent -notmatch "CLSCompliant" ) {
+
+				$newFileContent = $newFileContent + "`r`n[assembly: CLSCompliant( isCompliant: true )]`r`n"
+			}
+		}
+		Else {
+			
+			If( $newFileContent -notmatch "using System.Globalization;" ) {
+			
+				If( $newFileContent -match "using System\.Collections\.Generic;" ) {
+				
+					$newFileContent = $newFileContent -replace "using System\.Collections\.Generic;", "using System.Collections.Generic;`r`nusing System.Globalization;"
+				}
+				Else {
+				
+					# add it to the start of the file:
+					$newFileContent = $newFileContent.Replace( "using System;`r`n", "" )
+					$newFileContent = "using System;`r`nusing System.Globalization;`r`n" + $newFileContent
+				}
+			}
+
+			If( $newFileContent -notmatch "using System.Text;" ) {
+			
+				If( $newFileContent -match "using System\.Threading\.Tasks;" ) {
+				
+					$newFileContent = $newFileContent -replace "using System\.Threading\.Tasks;", "using System.Text;`r`nusing System.Threading.Tasks;"
+				}
+				Else {
+				
+					# add it to the start of the file:
+					$newFileContent = $newFileContent.Replace( "using System;`r`n", "" )
+					$newFileContent = "using System;`r`nusing System.Text;`r`n" + $newFileContent
+				}
+			}
+
+		}
+
+		#$newFileContent | Out-File $fileInfo.FullName -encoding ascii # using ASCII encoding instead of UTF8 to avoid BOM. Using non-BOM UTF8 in powershell is non-trivial.
+
+		[System.IO.File]::WriteAllText( $fileInfo.FullName, $newFileContent, $encoding )
+
+		$script:fileCount = $script:fileCount + 1
+
+	}
+	End { }
+}
+
+# from https://vertigion.com/2015/02/04/powershell-get-fileencoding/
+function Get-FileEncoding {
+	[CmdletBinding()]
+	param (
+		[Alias("PSPath")]
+		[Parameter(Mandatory = $True, ValueFromPipelineByPropertyName = $True)]
+		[String]$Path
+		,
+		[Parameter(Mandatory = $False)]
+		[System.Text.Encoding]$DefaultEncoding = $null
+	)
+	
+	process {
+		[Byte[]]$bom = Get-Content -Encoding Byte -ReadCount 4 -TotalCount 4 -Path $Path
+		
+		$encoding_found = $false
+		
+		foreach ($encoding in [System.Text.Encoding]::GetEncodings().GetEncoding()) { # how does this work? `GetEncodings()` returns an array, so how does the subsequent `GetEncoding()` call work?
+			$preamble = $encoding.GetPreamble()
+			if ($preamble) {
+				foreach ($i in 0..$preamble.Length) {
+					if ($preamble[$i] -ne $bom[$i]) {
+						break
+					} elseif ($i -eq $preamble.Length) {
+						$encoding_found = $encoding
+					}
+				}
+			}
+		}
+		
+		if (!$encoding_found) {
+			$encoding_found = $DefaultEncoding
+		}
+	
+		$encoding_found
+	}
+}
+
+# Create directory:
+New-Item -ItemType Directory -Force -Path "VS2017Templates"
+cd "VS2017Templates"
+$workingDirectoryPath = Resolve-Path "."
+
+If( $useGit ) {
+	git init
+}
+
+$itemTemplates = Join-Path $vsCommon7IDE "ItemTemplates"
+$projTemplates = Join-Path $vsCommon7IDE "ProjectTemplates"
+
+Copy-Item -Path $itemTemplates -Filter *.cs -Destination "ItemTemplates"    -Recurse
+Copy-Item -Path $projTemplates -Filter *.cs -Destination "ProjectTemplates" -Recurse
+
+If( $useGit ) {
+	git add *
+	git commit -m "Initial state of C# templates from Visual Studio."
+}
+
+Get-ChildItem -Path . -File -Filter *.cs -Recurse | Update-CSharpItemTemplate -workingDirectoryPath $workingDirectoryPath
+
+If( $useGit ) {
+	git add *
+	git commit -m "After updating templates."
+}
+
+Write-Host "$fileCount files were updated. You need to manually (with elevated permissions) overwrite the original template files in `"$vsCommon7IDE`"."
